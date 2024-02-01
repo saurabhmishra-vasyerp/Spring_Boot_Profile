3d3ad44ae598be664f77d0c4d22f22930cfc5c28
saurabhmishra <saurabhmishra@SODEV-SAURABHM.VASYERP.LOCAL>
2024-02-01 12:56:12 +0530

    commit and push

--- src/main/java/com/example/Student/service/StudentServiceimpl.java
+++ src/main/java/com/example/Student/service/StudentServiceimpl.java
@@ -1,9 +1,14 @@
 package com.example.Student.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Student.dto.StudentDto;
 import com.example.Student.model.StudentDetail;
 import com.example.Student.repository.StudentRepo;
 
@@ -20,6 +25,20 @@ public class StudentServiceimpl implements StudentService {
 	}
 
 	@Override
+	public List<StudentDto> getData() {
+		List<StudentDetail> studentDetails = studentRepo.findAll();
+		List<StudentDto> studentDtos = new ArrayList<StudentDto>();
+		for (StudentDetail s : studentDetails) {
+			StudentDto studentDto = new StudentDto();
+			studentDto.setId(s.getId());
+			studentDto.setName(s.getName());
+			studentDto.setEmail(s.getEmail());
+			studentDtos.add(studentDto);
+
+		}
+		return studentDtos;
+	}
+
 	public StudentDetail getStudent() {
 		// TODO Auto-generated method stub
 		StudentDetail studentDetail1 = new StudentDetail();
@@ -47,12 +66,46 @@ public class StudentServiceimpl implements StudentService {
 	@Override
 	public boolean editPage(StudentDetail studentDetail) {
 		// TODO Auto-generated method stub
-		System.err.println("hekko");
+		System.err.println("helloo");
 		StudentDetail edDetail = studentRepo.save(studentDetail);
-		if (studentRepo.findById(edDetail.getId())!= null) {
+		if (studentRepo.findById(edDetail.getId()) != null) {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public void updateStudent(int id, StudentDetail updatedStudent) {
+		// Implementation for updating student
+		StudentDetail existingStudent = studentRepo.findById(id).orElse(null);
+
+		if (updatedStudent.getName() != null) {
+			existingStudent.setName(updatedStudent.getName());
+		}
+		if (updatedStudent.getEmail() != null) {
+			existingStudent.setEmail(updatedStudent.getEmail());
+		}
+		if (updatedStudent.getAddress() != null) {
+			existingStudent.setAddress(updatedStudent.getAddress());
+		}
+		if (updatedStudent.getGender() != null) {
+			existingStudent.setGender(updatedStudent.getGender());
+		}
+		if (updatedStudent.getPhone() != null) {
+			existingStudent.setPhone(updatedStudent.getPhone());
+		}
+
+		studentRepo.save(existingStudent);
+
+//	        else {
+//	            throw new EntityNotFoundException("Student with ID " + updatedStudent.getId() + " not found");
+//	        }
+	}
+
+	@Override
+	public void saveStudent(StudentDetail studentDetail) {
+		// Implementation for saving a student
+		studentRepo.save(studentDetail);
 	}
 
 }
